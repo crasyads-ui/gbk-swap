909e87ccfdcfb24da0669e47674346d6cef240cb
crasyads-ui <crasyads@gmail.com>
2026-09-14 05:26:56 +0530

    Update countries.js

--- api/fiat/countries.js
+++ api/fiat/countries.js
@@ -1,19 +1,20 @@
 export default function handler(req, res) {
-  const currencies = [
-    { code: "USD", name: "US Dollar", symbol: "$" },
-    { code: "EUR", name: "Euro", symbol: "€" },
-    { code: "GBP", name: "British Pound", symbol: "£" },
-    { code: "INR", name: "Indian Rupee", symbol: "₹" },
-    { code: "AED", name: "UAE Dirham", symbol: "د.إ" },
-    { code: "SGD", name: "Singapore Dollar", symbol: "S$" },
-    { code: "CAD", name: "Canadian Dollar", symbol: "C$" },
-    { code: "AUD", name: "Australian Dollar", symbol: "A$" },
-    { code: "JPY", name: "Japanese Yen", symbol: "¥" },
-    { code: "CNY", name: "Chinese Yuan", symbol: "¥" }
+  const countries = [
+    { alpha2: "IN", name: "India", currencyCode: "INR", isAllowed: true },
+    { alpha2: "US", name: "United States", currencyCode: "USD", isAllowed: true },
+    { alpha2: "GB", name: "United Kingdom", currencyCode: "GBP", isAllowed: true },
+    { alpha2: "AE", name: "United Arab Emirates", currencyCode: "AED", isAllowed: true },
+    { alpha2: "SG", name: "Singapore", currencyCode: "SGD", isAllowed: true },
+    { alpha2: "CA", name: "Canada", currencyCode: "CAD", isAllowed: true },
+    { alpha2: "AU", name: "Australia", currencyCode: "AUD", isAllowed: true },
+    { alpha2: "JP", name: "Japan", currencyCode: "JPY", isAllowed: true },
+    { alpha2: "CN", name: "China", currencyCode: "CNY", isAllowed: true },
+    { alpha2: "DE", name: "Germany", currencyCode: "EUR", isAllowed: true },
+    { alpha2: "FR", name: "France", currencyCode: "EUR", isAllowed: true }
   ];
 
   res.status(200).json({
     success: true,
-    currencies
+    countries
   });
 }
